3eb400cab71beb8660da00fc6fe301c6c0c1a87e
realganfan <73422849+realganfan@users.noreply.github.com>
2020-11-08 01:15:42 +0800

    Fix missing files issue

---
+++ src/gcc-4.8.0/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+cd $(dirname $0)
+. ../common/functions
+
+TARGET=$(tc_target $1)
+SRCDIR=$(pwd)
+
+case $TARGET in
+i686-linux)
+   $SRCDIR/build.sh i686-linux
+   $SRCDIR/build.sh x86_64-linux
+   ;;
+
+x86_64-linux)
+   $SRCDIR/build.sh x86_64-linux
+   ;;
+esac
